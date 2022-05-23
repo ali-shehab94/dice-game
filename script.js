c62bdae2c2c4f8877d7ec8e08d0185e228231dc8
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-05-23 11:27:12 +0300

    set img scr for both images to change according to random number

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let dice1 = Math.floor(Math.random() * 7) + 1;
-let dice2 = Math.floor(Math.random() * 7) + 1;
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+console.log(dice1, dice2)
 
-console.log(dice1, dice2);
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+
+let imgName1 = "dice" + dice1;
+let imgName2 = "dice" + dice2;
+console.log(imgName1, imgName2)
+
+img1.src = "assets/images/" + imgName1 + ".png"
+img2.src = "assets/images/" + imgName2 + ".png"
+console.log(img1.src, img2.src)
