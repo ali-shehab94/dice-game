577ea587f9ee01133f79567288c33f609f8e2de5
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-05-23 11:48:48 +0300

    added functionality to the display text

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let dice1 = Math.floor(Math.random() * 6) + 1;
 let dice2 = Math.floor(Math.random() * 6) + 1;
 console.log(dice1, dice2)
 
+let displayText = document.getElementById("display-text")
+if (dice1 > dice2) {
+    displayText.innerText = ("🚩Player 1 Wins!")
+}else if (dice1 < dice2) {
+    displayText.innerText = ("🚩Player 2 Wins!")
+}else {
+    displayText.innerText = ("🎌 Draw 🎌")
+}
+
 let img1 = document.getElementById("img1");
 let img2 = document.getElementById("img2");
 
@@ -12,3 +21,4 @@ console.log(imgName1, imgName2)
 img1.src = "assets/images/" + imgName1 + ".png"
 img2.src = "assets/images/" + imgName2 + ".png"
 console.log(img1.src, img2.src)
+
